52a49d2cf16abcaba938fe00e9c3db4b8996b5f2
Alexander Hill <alexanderhill4130@gmail.com>
2022-06-15 19:18:29 +0200

    Added CRUD functionality for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :contacts
   # get 'home/index'
-  root 'home#index'
 
+  root 'home#index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220615165635_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :twitter
+
+      t.timestamps
+    end
+  end
+end
