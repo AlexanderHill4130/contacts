b10cc89d9b4d936abf73e01cc1f54a4671e3130d
Alexander Hill <alexanderhill4130@gmail.com>
2022-06-16 18:25:20 +0200

    Added authentication for user sessions and styling tweaks

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,7 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /contacts or /contacts.json
   def index
@@ -12,7 +14,8 @@ class ContactsController < ApplicationController
 
   # GET /contacts/new
   def new
-    @contact = Contact.new
+    # @contact = Contact.new
+    @contact = current_user.contacts.build
   end
 
   # GET /contacts/1/edit
@@ -21,7 +24,8 @@ class ContactsController < ApplicationController
 
   # POST /contacts or /contacts.json
   def create
-    @contact = Contact.new(contact_params)
+    # @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
 
     respond_to do |format|
       if @contact.save
@@ -55,6 +59,11 @@ class ContactsController < ApplicationController
       format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @contact = current_user.contacts.find_by(id: params[:id])
+    redirect_to contacts_path, notice: "Not Authorised to make changes to this contact" if @contact.nil?
   end
 
   private
